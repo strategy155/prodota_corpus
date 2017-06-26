dd5b250b97ebbd7020f8507b38249dc0a317cacf
GaurwaithMelui <strategy155@gmail.com>
2017-06-26 16:39:54 +0300

    created source code requests getter, created main procedure

--- test/test_site_parsing.py
+++ test/test_site_parsing.py
@@ -1,7 +1,21 @@
 import bs4
 import src.definitions
+import requests
 
+
+URL = 'prodota.ru'
+
+
+def get_source_code(url):
+    requests_object = requests.get(url)
+    source_code = requests_object.text
+    return source_code
 
 def get_html_structure(webpage_source_code):
     soup = bs4.BeautifulSoup(webpage_source_code, src.definitions.DEFAULT_PARSER)
     print(soup.prettify())
+
+
+if __name__ == '__main__':
+    source_code = get_source_code(URL)
+    get_html_structure(source_code)
